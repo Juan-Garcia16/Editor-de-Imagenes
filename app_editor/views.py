2860cc71f43299628346ef6dd0d5895fb304e22f
Juan Garcia <juan.garcia16@utp.edu.co>
2025-10-05 17:01:38 -0500

    carga de imagenes funcional

--- app_editor/views.py
+++ app_editor/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage # Importa FileSystemStorage para manejar archivos
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    imagen_url = None
+    # Si se envió un formulario
+    if request.method == 'POST' and request.FILES.get('imagen'):
+        imagen = request.FILES['imagen']
+        fs = FileSystemStorage()  # usa MEDIA_ROOT automáticamente
+        nombre_archivo = fs.save(imagen.name, imagen)
+        imagen_url = fs.url(nombre_archivo)  # genera /media/nombre.png
+        
+    return render(request, 'index.html', {'imagen_url': imagen_url})
--- editor/urls.py
+++ editor/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', include('app_editor.urls')),
     path('admin/', admin.site.urls),
 ]
+
+# Configuración para servir archivos multimedia en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
